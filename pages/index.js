d82b604601f9801386cc5112b88dbb21cb2747ba
Vlad Mamonov <mmvvlad@gmail.com>
2022-02-15 18:39:46 +0700

    Добавил Header На сайт

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -10,9 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lets build Hulu</h1>
-
       {/* Header */}
+      <Header />
 
       {/* Navbar */}
 
